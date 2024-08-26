da5f5b45699342aad2c31cc56bc1b1d8e76ce058
Vika <vikapetrova8@gmail.com>
2024-08-26 23:01:56 +0300

    Container

---
+++ src/components/Container.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1280px; //Света говорит нужно ставить до 1200?
+    width: 100%;
+    min-height: 100%; //на всю высоту родителя
+    padding: 0 40px;
+    margin: 0 auto;
+    border: 1px solid blueviolet;
+`
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from "styled-components";
-import { Logo } from '../../components/logo/Logo';
-import { Menu } from '../../components/menu/Menu';
+import {Logo} from '../../components/logo/Logo';
+import {Menu} from '../../components/menu/Menu';
+import {Container} from '../../components/Container';
+import {FlexWrapper} from "../../components/FlexWrapper";
 
 const items = ['Home', 'About', 'Works', 'Blogs', 'Testimonials', 'Contact']
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                <Logo/>
+                <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
     background-color: lemonchiffon;
-    display: flex;
-    justify-content: space-between;
 `
