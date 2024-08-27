c8a3ebaa00970e7397e6583c89e11ce34ed5b2b3
Vika <vikapetrova8@gmail.com>
2024-08-27 23:50:38 +0300

    Main styles

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,14 +3,18 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import styled from "styled-components";
 import {Button} from "../../../components/Button";
 import mainPhoto from '../../../assets/images/main-photo.webp'
+import {Container} from "../../../components/Container";
+
 export const Main = () => {
     return (
         <StyledMain>
-            <span>hi! everyone</span>
-            <h1>Chris Lee <br/>
-                brand designer</h1>
-            <p>Make designs mainly logos, visual identities, apps & websites, social media and magazines.</p>
-            <Button>get in touch</Button>
+            <Container>
+                <SmallText>hi! everyone</SmallText>
+                <MainTitle>Chris Lee <br/>
+                    brand designer</MainTitle>
+                <Text>Make designs mainly logos, visual identities, apps & websites, social media and magazines.</Text>
+                <Button>get in touch</Button>
+            </Container>
         </StyledMain>
     );
 };
@@ -18,5 +22,29 @@ export const Main = () => {
 const StyledMain = styled.section`
     //background-color: powderblue;
     min-height: 100vh;
-    text-align: center;
+    text-align: left;
+`
+
+const SmallText = styled.span`
+    font-family: "Inconsolata", monospace;
+    font-weight: 400;
+    font-size: 18px;
+    letter-spacing: 0.24em;
+    text-transform: uppercase;
+    padding: 4px 12px;
+    background: rgba(236, 236, 236, 0.17);
+`
+
+const MainTitle = styled.h1`
+    font-weight: 800;
+    font-size: 108px;
+    letter-spacing: -0.04em;
+    text-transform: capitalize;
+    margin: 45px 0 35px;
+`
+
+const Text = styled.p`
+    font-weight: 400;
+    font-size: 24px;
+    letter-spacing: 0.02em;
 `
