4383d0e00bfac2ab4cb885447186cc17f2bcde18
Vika <vikapetrova8@gmail.com>
2024-09-07 14:14:03 +0300

    Achievement styles

--- src/layout/sections/achievements/Achievements.tsx
+++ src/layout/sections/achievements/Achievements.tsx
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Achievement} from "./achievement/Achievement";
+import {Container} from "../../../components/Container";
 
 export const Achievements = () => {
     return (
         <StyledAchievements>
-            <SectionTitle>Achievements</SectionTitle>
-            <FlexWrapper wrap={'wrap'}>
+            <Container>
+            <SectionTitle extraText={'awards and recognition'}>Achievement</SectionTitle>
+            <FlexWrapper wrap={'wrap'} justify={'space-between'}>
                 <Achievement title={'Interior design'} text={'Breakthrough designer of the year 2020'}/>
                 <Achievement title={'Interior design'} text={'Breakthrough designer of the year 2020'}/>
                 <Achievement title={'Interior design'} text={'Breakthrough designer of the year 2020'}/>
@@ -16,12 +18,15 @@ export const Achievements = () => {
                 <Achievement title={'Interior design'} text={'Breakthrough designer of the year 2020'}/>
                 <Achievement title={'Interior design'} text={'Breakthrough designer of the year 2020'}/>
             </FlexWrapper>
+            </Container>
         </StyledAchievements>
     );
 };
 
 const StyledAchievements = styled.section`
-    background-color: lavender;
-    min-height: 100vh;
+    margin: 146px 0 168px; //нижний марджин прибавлен на 40px из-за отступа у псевдоэлемента в минус 40px
+    ${FlexWrapper} {
+        row-gap: 50px;
+    }
 `
 
--- src/layout/sections/achievements/achievement/Achievement.tsx
+++ src/layout/sections/achievements/achievement/Achievement.tsx
@@ -9,12 +9,41 @@ type AchievementPropsType = {
 export const Achievement = (props: AchievementPropsType) => {
     return (
         <StyledAchievement>
-            <h3>{props.title}</h3>
-            <p>{props.text}</p>
+            <Title>{props.title}</Title>
+            <Text>{props.text}</Text>
         </StyledAchievement>
     );
 };
 
 const StyledAchievement = styled.div`
-    width: 33%;
+    width: 30%;
+`
+
+const Title = styled.h3`
+    font-weight: 500;
+    font-size: 22px;
+    text-transform: capitalize;
+    color: #111;
+    margin: 55px 0 5px;
+`
+
+const Text = styled.p`
+    font-weight: 300;
+    font-size: 19px;
+    line-height: 188%;
+    letter-spacing: 0.02em;
+    color: #282828;
+    
+    position: relative;
+    
+    &::before {
+        content: '';
+        display: inline-block;
+        position: absolute;
+        height: 1px;
+        width: 100%;
+        bottom: 0;
+        margin-bottom: -40px;
+        background-color: #e2e2e2;
+    }
 `
